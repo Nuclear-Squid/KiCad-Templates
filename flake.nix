4350c7fdcb43097e0412c7605f499e4905eed170
Nuclear Squid <leo@cazenave.cc>
2025-10-12 14:01:04 +0200

    Added a very basic Fix Flake.

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nix flake for the KiCad plugin development";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    self-pkgs = self.packages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        (python3.withPackages (py-pkgs: with py-pkgs; [ uv ]))
+      ];
+    };
+  };
+}
