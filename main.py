b7e78ae0e8ef2f5da005982328cb48fa0be264bd
Léo Cazenave <70967142+Nuclear-Squid@users.noreply.github.com>
2025-10-12 16:35:13 +0000

    Improved ci: `ruff format` + `ty check`, closes #1. (#2)
    * Improved ci: `ruff format` + `ty check`, closes #1.
    
    * Fix errors reported by `ty`.
    
    * oops

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def test(x: str) -> str:
+    return "string: " + x
+
 def main():
-    print("Hello from kicad-templates!")
+    print(   "Hello from kicad-templates!")
+    print(test('test to make sure the CI works well'))
 
 
 if __name__ == "__main__":
