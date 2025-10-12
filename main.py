0cb1992956154d60cecce41dc38423a63206d0e7
Léo Cazenave <70967142+Nuclear-Squid@users.noreply.github.com>
2025-10-12 16:46:26 +0000

    Fix code formatter check in ci (#3)
    * oops, again
    
    * Fixed dummy formatting error.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def test(x: str) -> str:
     return "string: " + x
 
+
 def main():
-    print(   "Hello from kicad-templates!")
-    print(test('test to make sure the CI works well'))
+    print("Hello from kicad-templates!")
+    print(test("test to make sure the CI works well"))
 
 
 if __name__ == "__main__":
