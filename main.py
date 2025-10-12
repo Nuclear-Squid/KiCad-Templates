dab383de188b32b0fc7dc3a40b89a382b8e0f56d
Nuclear Squid <leo@cazenave.cc>
2025-10-12 18:33:21 +0200

    Fix errors reported by `ty`.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def test(x: float) -> float:
-    return x + "string"
+def test(x: string) -> float:
+    return "string: " + x
 
 def main():
     print(   "Hello from kicad-templates!")
-    print(test('prout'))
+    print(test('test to make sure the CI works well'))
 
 
 if __name__ == "__main__":
