3b602a537df4bf6e2f07fceaaf464a790c188579
Léo Cazenave <70967142+Nuclear-Squid@users.noreply.github.com>
2026-02-23 14:30:30 +0000

    Yaml metadata + CLI project creation. (#24)
    * wip
    
    * Yaml metadata + CLI project creation.

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 import click
 
-from kicad_api import KiCadAPI, KiCadLibrary
-from hierarchical_object import HierarchicalObject
-from templates import SCH_Templates
+from schematic_api.kicad_api import KiCadAPI, KiCadLibrary
+import schematic_api.templates as templates
 
 PROJECT_FOLDER = Path(__file__).parent.parent
-SUBSYSTEM_FOLDER = PROJECT_FOLDER / 'subsystems'
+SUBSYSTEM_FOLDER = PROJECT_FOLDER / "subsystems"
 
 DEVICE_LIB_PATH = "/usr/share/kicad/symbols/Device.kicad_sym"  # Change it somehow
+
 
 # Define a group for all of our CLI commands
 @click.group()
@@ -28,41 +28,27 @@ def list():
 
 # Create new project with specified templates
 @cli.command()
-@click.argument('project_name')
-@click.argument('templates', nargs=-1)
-def new(project_name: str, templates: tuple[str, ...]):
+@click.argument("project_name")
+@click.argument("template_names", nargs=-1)
+def new(project_name: str, template_names: tuple[str, ...]):
     api = KiCadAPI()
-    lib = KiCadLibrary()
+    subsystems = templates.load_templates(SUBSYSTEM_FOLDER)
+
+    # TODO: limit project name to valid characters and length for KiCad
+    for character in project_name:
+        if not character.isalnum() and character not in ('-', '_'):
+            print("Erreur: le nom du projet ne doit contenir que des lettres, chiffres, tirets ou underscores.")
+            return
 
     blocks = []
-    for t in templates:
-        sheet = SCH_Templates.find_sheet_folder(t)
-        if sheet is None:
-            click.echo(f"Warning: could not find template '{t}'.")
-        else:
-            blocks.append(sheet)
+    for name in template_names:
+        t = templates.find_template(name, subsystems)
+        if t is None:
+            click.echo(click.style("Error: ", fg="red") + f"Could not find template '{name}'")
+            return
+        blocks.append(t)
 
-    # Charger un schématique
-    schematic = api.load_schematic("test_python.kicad_sch")
-
-    # Block placement
-    placed = schematic.add_hierarchical_sheets(
-        blocks,
-        origin_xy = (33, 20),
-        max_row_width_mm = 200,   # controla quantos cabem por linha
-        h_gap_factor = 0.8,       # gap horizontal proporcional ao tamanho
-        v_gap_factor = 1.0,       # gap vertical proporcional à altura
-        page_for_instance_start = 10   # evita conflito com páginas anteriores
-    )
-
-    print("SHEETS PLACED:")
-    for p in placed:
-        print(p)
-
-    schematic.export_schematic("test_python.kicad_sch")
-
-    print("File generated: test_python.kicad_sch")
-
+    api.project_creation(project_name, blocks)
 
 if __name__ == "__main__":
     cli()
---
+++ src/schematic_api/templates.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from schematic_api.hierarchical_object import HierarchicalObject
+
+def load_templates(templates_folder: Path) -> list[HierarchicalObject]:
+    result = []
+    for path in templates_folder.iterdir():
+        template = HierarchicalObject.load_from_yaml(path / "meta.yaml")
+        if template is None:
+            print(f"Warning: could not load template '{template}'")
+            continue
+
+        result.append(template)
+    return result
+
+
+def find_template(name: str, templates: list[HierarchicalObject]) -> HierarchicalObject | None:
+    for t in templates:
+        if t.dev_name == name:
+            return t
+    return None
