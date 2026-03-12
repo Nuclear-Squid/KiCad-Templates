9b8e05a805d6b90d3c37b442f86bf888dd671344
Nuclear Squid <leo@cazenave.cc>
2026-03-12 10:39:09 +0100

    More compact pin placement.

--- src/schematic_api/schematic.py
+++ src/schematic_api/schematic.py
@@ -6,7 +6,6 @@ from typing import Self, TYPE_CHECKING
 from uuid import uuid4
 
 from kicad_sexp import KiCadSexpNode
-from schematic_api.hierarchical_object import HierarchicalObject
 # from schematic_api.templates import TemplateMetadata
 
 Number = int | float
@@ -46,11 +45,11 @@ class KiCadSchematic:
 
 
     @classmethod
-    def new_sheet(cls, sheet_name, sheet_file, w, h, x, y) -> Self:
+    def new_sheet(cls, sheet_name, sheet_file, width, height, x, y) -> Self:
         return KiCadSexpNode.from_sexpdata([
             "sheet",
             ["at", x, y],
-            ["size", w, h],
+            ["size", width, height],
             ["fields_autoplaced"],
             ["stroke",
                 ["width", 0.1524],
@@ -97,56 +96,33 @@ class KiCadSchematic:
         page_for_instance: str = "2",
         net_wire_len_mm: float = 5.0,    # length of wire from pin to net label
     ):
-        w = float(metadata.size_wh[0])
-        h = float(metadata.size_wh[1])
+        width = float(metadata.size_wh[0])
+        height = float(metadata.size_wh[1])
 
-        x_left  = position[0]
-        x_right = position[0] + w
-        y_top   = position[1]
-        y_bot   = position[1] + h
-
-        props = metadata.properties or {}
-
-        left_pins  = metadata.left_pins
-        right_pins = metadata.right_pins
-
-        #   2) Builds the block (sheet ...)
         sheet = self.new_sheet(
             metadata.sheet_name,
             metadata.sheet_file,
-            w,
-            h,
+            width,
+            height,
             position[0],
             position[1]
         )
 
         # Extra properties
-        for k, v in props.items():
+        for k, v in metadata.properties.items():
             sheet.append(KiCadSexpNode.make_property(k, v, position, hide=True))
 
-        pin_indexes_left  = range(len(left_pins))
-        pin_indexes_right = range(len(right_pins))
+        def place_pins(pin_list, pos_x, pos_y, label_x_offset):
+            start_height = position[1] + (height - len(pin_list)  * 2.54)  / 2
+            for i, pin in enumerate(pin_list):
+                pos_y = start_height + i * 2.54
+                x_end = pos_x + float(label_x_offset)
+                sheet.append(KiCadSexpNode.make_pin(pin.name, pin.type, pos_x, pos_y))
+                self.data.append(KiCadSexpNode.make_wire(pos_x, pos_y, x_end, pos_y))
+                self.data.append(KiCadSexpNode.make_label(pin.net, x_end, pos_y, "right"))
 
-        start_height_left  = position[1] + (h - len(left_pins)  * 2.54)  / 2
-        start_height_right = position[1] + (h - len(right_pins) * 2.54) / 2
-
-        ys_left  = map(lambda x: x * 2.54 + start_height_left,  pin_indexes_left)
-        ys_right = map(lambda x: x * 2.54 + start_height_right, pin_indexes_right)
-
-        # Left: angle 180
-        for pin, y in zip(left_pins, ys_left):
-            x_end = x_left - float(net_wire_len_mm)
-            sheet.append(KiCadSexpNode.make_pin(pin.name, pin.type, x_left, y))
-            self.data.append(KiCadSexpNode.make_wire(x_left, y, x_end, y))
-            self.data.append(KiCadSexpNode.make_label(pin.net, x_end, y, "right"))
-
-        # Right: angle 0
-        for p, y in zip(right_pins, ys_right):
-            x_end = x_right + float(net_wire_len_mm)
-            sheet.append(KiCadSexpNode.make_pin(pin.name, pin.type, x_right, y))
-            self.data.append(KiCadSexpNode.make_wire(x_right, y, x_end, y))
-            self.data.append(KiCadSexpNode.make_label(pin.net, x_end, y, "left"))
-
+        place_pins(metadata.left_pins,  position[0], position[1], -net_wire_len_mm)
+        place_pins(metadata.right_pins, position[0] + width, position[1],  net_wire_len_mm)
 
         # # ---- Inserção do sheet ----
         # self.data.append_sexp(sheet)
